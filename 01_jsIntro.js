e22ee2e8334a8d614d83de30511240f6528a5101
Ronnoci <cmcin39@gmail.com>
2019-05-14 09:25:10 +1000

    Completed

--- 01_jsIntro.js
+++ 01_jsIntro.js
@@ -10,11 +10,15 @@
 // Run 'mocha 01_jsIntro.js' to test 
 // (you'll have to have mocha installed globally)
 
-// Define global variables here
 
-function setNumbers(num) {
-  // Your code here
+// Define global variables here
+  const myString = 'my string'
+  let numbers = 7
+
+function setNumbers(num) {  
+  numbers = num
 }
+
 var assert = require('assert');
 
 describe('jsIntro', () => {
